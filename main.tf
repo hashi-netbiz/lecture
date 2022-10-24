97d13f85fba10fd02c0b5445f4205caa0da8ce97
hashi-netbiz <116410436+hashi-netbiz@users.noreply.github.com>
2022-10-24 18:35:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,57 @@
 terraform {
-  required_version = ">=1.2.0"
+  required_version = ">= 1.3"  //latest terraform version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+}
+}
+# Create Random Pet Resource
+resource "random_pet" "this" {
+  length = 1
+  prefix = "hashi"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "myvpc"
+    team = "devops"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = random_pet.this.id
+  }
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-09d3b3274b6c5d4aa"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.my_subnet.id
+
+    tags = {
+    Name = random_pet.this.id
+  }
+}
+
+output "petname" {
+  value = random_pet.this.id
 }
